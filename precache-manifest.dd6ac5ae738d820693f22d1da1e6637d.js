a69d979f33866f884a046ebc4dd5049d8f1df2c6
Abd Alwahab <bayanadam10@gmail.com>
2019-08-17 08:34:45 +0300

    Updates

---
+++ precache-manifest.dd6ac5ae738d820693f22d1da1e6637d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2585ee80bc4f72cf4fdde12efe167c23",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "304d309cda1c98f95a62",
+    "url": "/learn-react/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "a184da77b37f97247688",
+    "url": "/learn-react/static/js/2.f05352c2.chunk.js"
+  },
+  {
+    "revision": "304d309cda1c98f95a62",
+    "url": "/learn-react/static/js/main.fbd13e98.chunk.js"
+  },
+  {
+    "revision": "5c586eafeeebbfbdfcaa",
+    "url": "/learn-react/static/js/runtime~main.a01a4c71.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/learn-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
